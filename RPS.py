8b932c211c0297d5e3668bf8daa29d9a0b04aa90
Faith Best <97316553+ibringfaith@users.noreply.github.com>
2024-12-29 17:04:12 -0500

    Improve player function

--- RPS.py
+++ RPS.py
@@ -1,10 +1,22 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
-
+# The example function kept track of the opponent's history and played whatever the opponent played two plays ago. It is not a good player so I changed the code to pass the challenge.
+# Keep track of patterns in the opponent's history and play whatever the opponent is most likely to play based on their previous playing history.
 def player(prev_play, opponent_history=[]):
     opponent_history.append(prev_play)
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    plays = { "RR": 0, "RP": 0, "RS": 0, "PR": 0, "PP": 0, "PS": 0, "SR": 0, "SP": 0, "SS": 0 }
+
+    #guess = "R"
+    #if len(opponent_history) > 2:
+    #    guess = opponent_history[-2]
+
+    if len(opponent_history) > 1:
+        recent_plays = "".join(opponent_history[-2:])
+        plays[recent_plays] += 1
+        
+    potential_plays = [prev_play + "R", prev_play + "P", prev_play + "S"]
+    predicted_next_play = max(potential_plays, key=lambda play: plays.get(play))[-1]
+
+    counter_play = {"R": "P", "P": "S", "S": "R"}
+    guess = counter_play[predicted_next_play]
 
     return guess
